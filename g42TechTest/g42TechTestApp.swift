76ca6f44e27dd9da7b61a970016ea51705e7b462
Azhar Islam <azhartislam@gmail.com>
2022-06-24 23:59:50 +0800

    Initial Commit

---
+++ g42TechTest/g42TechTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  g42TechTestApp.swift
+//  g42TechTest
+//
+//  Created by Azhar Islam on 24/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct g42TechTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
